571129c4801fdae76070da6eccc1d762069ba1da
NinaCalvi <nina@calvifamily.org>
2020-05-06 09:53:20 +0100

    metrics

--- eval/metrics.py
+++ eval/metrics.py
@@ -1,13 +1,50 @@
 #contains possible different metrics
+from sklearn.metrics import roc_auc_score, average_precision_score
+
 
 def rank(y_pred, true_idx):
+
+    '''
+    y_pred: np.array 2-dim of predictions - num instances x num labels
+    true_idx: np array of idx of true labels - 1d, (num_labels, )
+    '''
+
     #sorts from smallest to biggest
     #we want biggest first (i.e. highest score)
 
     #applying argsort again will undo the sort done before
     #an assign to each element in the list its rank within the sorted stuff
     order_rank  = np.argsort(np.argsort(-y_pred))
-    rank  = order_rank[true_idx] + 1
+    rank  = order_rank[np.arange(len(y_pred)), true_idx] + 1
     return rank
 
-def mean_rank(y_pred, true_idx):    
+def mean_rank(y_pred, true_idx):
+    '''
+    Compute the mean ranks
+
+    y_pred: np.array 2-dim of predictions - num instances x num labels
+    true_idx: np array of idx of true labels - 1d, (num_labels, )
+    '''
+    ranks = rank(y_pred, true_idx)
+    return np.mean(ranks)
+
+def mrr(y_pred, true_idx):
+    '''
+    Compute the mean reciprocal rank
+
+    y_pred: np.array 2-dim of predictions - num instances x num labels
+    true_idx: np array of idx of true labels - 1d, (num_labels, )
+    '''
+    reciprocal = 1/rank(y_pred, true_idx)
+    return np.mean(reciprocal)
+
+
+def auc_roc(y_pred, true_idx):
+    '''
+    Compute the area under the ROC curve
+
+    y_pred: np.array 2-dim of predictions - num instances x num labels
+    true_idx: np array of idx of true labels - 1d, (num_labels, )
+    '''
+    #first is to translate the ture label idx in matrix of num_instance x num_labels
+    #this matrix will be binary, 1 at the label index and 0 everywhere else. 
