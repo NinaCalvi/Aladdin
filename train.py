200ff04f3737c71a593d96cc943f33bdea2225d0
NinaCalvi <nina@calvifamily.org>
2020-05-06 17:35:03 +0100

    framework start

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from embeddings import KBCModel, KBCModelMCL
 from torch import nn
 from torch import optim
 from argparse import Namespace
+import numpy as np
 
 def train(model: nn.Module,
         regulariser: string,
@@ -30,11 +31,35 @@ def train_not_mc():
     pass
 
 def train_mc(model: KBCModel, regulariser: string, optimiser: optim.Optimizer, data: torch.Tensor, args: Namespace):
+
+
     nb_negs = args.nb_negs
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     batch_size = args.batch_size
     emb_size = args.emb_size
-    nb_epochs = args.nb_eppchs
+    nb_epochs = args.nb_epochs
+    seed = args.seed
+
+    #set seed
+    np.random.seed(seed)
+    random_state = np.random.RandomState(seed)
+    torch.manual_seed(seed)
+
+    #the embeddings should be initialised with the model that has been passed on
+    inputs = data[torch.randperm(data.shape[0]),:]
+    for epoch in range(nb_epochs):
+        batch_start = 0
+        while batch_start < data.shape[0]:
+            batch_end = min(batch_start + batch_size, data.shape[0])
+            input_batch = input[batch_start:batch_end].to(device)
+            
+
+
 
 
     pass
