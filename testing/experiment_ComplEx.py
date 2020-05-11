de233e70b6457ce0d7e2c5dd3f8083593aa50353
NinaCalvi <nina@calvifamily.org>
2020-05-11 16:22:59 +0100

    starting training framework

--- testing/experiment_ComplEx.py
+++ testing/experiment_ComplEx.py
@@ -13,7 +13,7 @@ from ..embeddings import *
 from libkge import KgDataset
 
 
-def main():
+def main(argv):
     parser = argparse.ArgumentParser('BioLinkPred', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--data', action='store', required=True, type=str)
 
@@ -25,9 +25,38 @@ def main():
     parser.add_argument('--embedding-size', '-k', action='store', type=int, default=100)
     parser.add_argument('--batch-size', '-b', action='store', type=int, default=100)
 
+    parser.add_argument('--eval-batch-size', '-B', action='store', type=int, default=None)
+
+    # training params
+    parser.add_argument('--epochs', '-e', action='store', type=int, default=100)
+    parser.add_argument('--learning-rate', '-l', action='store', type=float, default=0.1)
+
+    parser.add_argument('--optimizer', '-o', action='store', type=str, default='adagrad',
+                        choices=['adagrad', 'adam', 'sgd'])
+
+    parser.add_argument('--loss', '-l', action='store', type=str, default='pw_logistic')
+    parser.add_argument('--regulariser', '-r', action='store', type=str, default='n3')
+    parser.add_argument('--reg-weight', action='store', type=float, required=True)
+
+    parser.add_argument('--seed', action='store', type=int, default=1234)
+    parser.add_argument('--quiet', '-q', action='store_true', default=False)
+
+
     data = parser.data
     emb_size = parser.embedding_size
     batch_size = parser.batch_size
+
+    nb_epochs = parser.epochs
+    lr = parser.learning_rate
+    optimizer_name = parser.optimizer
+    loss = parser.loss
+    regulariser = parser.regulariser
+    reg_weight = parser.reg_weight
+
+    seed = praser.seed
+    quiet = parser.quiet
+
+    args = parser.parse_args(argv)
 
     if data == 'pse':
         dataset = utils.load_pse_dataset()
@@ -42,6 +71,15 @@ def main():
     #THIS MAY BE ALSO BE DUE TO THE SYMMETRIC/AUGMENTATION SITUATION
 
     bench_idx_data = np.concatenate([train_data, valid_data, test_data])
+
+    optimizer_factory = {
+        'adagrad': lambda: optim.Adagrad(parameter_lst, lr=learning_rate),
+        'adam': lambda: optim.Adam(parameter_lst, lr=learning_rate),
+        'sgd': lambda: optim.SGD(parameter_lst, lr=learning_rate)
+    }
+
+    assert optimizer_name in optimizer_factory
+    optimizer = optimizer_factory[optimizer_name]()
 
     #need to pass size of embeddings
     if parser.model == 'complex':
