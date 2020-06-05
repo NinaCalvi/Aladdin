9ff737418ebb8282ef7598aefc38e3a8e9f1f028
NinaCalvi <nina@calvifamily.org>
2020-06-05 15:53:20 +0100

    attempt similar to kbc

--- biolink/eval/metrics.py
+++ biolink/eval/metrics.py
@@ -165,7 +165,7 @@ def evaluate(model: nn.Module, test_triples: torch.Tensor, all_triples: torch.Te
 
             #score triples
             queries_sp = model.get_queries(batch_tensor)
-            queries_po = model.get_queries(torch.index_select(batch_tensor, 1, torch.LongTensor[2,1,0]))
+            queries_po = model.get_queries(torch.index_select(batch_tensor, 1, torch.LongTensor([2,1,0])))
 
             scores_sp = queries_sp @ rhs
             scores_po = queries_po @ rhs
