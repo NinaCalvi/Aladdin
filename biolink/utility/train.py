c73137f618e49b639221bcdc63d75a23817755a0
NinaCalvi <nina@calvifamily.org>
2020-06-16 10:46:02 +0100

    valid

--- biolink/utility/train.py
+++ biolink/utility/train.py
@@ -109,7 +109,7 @@ def train_not_mc(model: KBCModel, regulariser_str: str, optimiser: optim.Optimiz
 
         if ((epoch % valid_every) == 0) and valid:
             logger.info(f'Validating')
-            val_metrics = evaluate(model, val_data, all_data, batch_size, device)
+            val_metrics = evaluate(model, valid_data, all_data, batch_size, device)
             if best_val_mrr is None:
                 best_val_mrr = val_metrics['MRR']
             elif best_val_mrr > val_metrics['MRR']:
