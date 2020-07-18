6e121f18627dfe8b51d4c4a3abc4a6980dea9db0
NinaCalvi <nina@calvifamily.org>
2020-07-18 15:31:03 +0100

    attempt bayesian tucker

--- biolink/testing/bayesian.py
+++ biolink/testing/bayesian.py
@@ -20,8 +20,8 @@ space = {'model': hp.choice('model', ['tucker']), \
         'learning-rate': hp.uniform('learning-rate', 0.0003, 1), \
         'batch-size': hp.choice('batch-size', [128, 268, 512]), \
         'reg-weight': hp.loguniform('reg-weight', np.log(1.0e-20), np.log(1.0e-01)), \
-        'emb-size': hp.uniform('emb-size', 50, 200), \
-        'rel-emb-size': hp.uniform('real-emb-size', 50, 200), \
+        'emb-size': hp.quniform('emb-size', 50, 200), \
+        'rel-emb-size': hp.quniform('real-emb-size', 50, 200), \
         'quiet': hp.choice('quiet', [True]), \
         'valid': hp.choice('valid', [True])}
 
