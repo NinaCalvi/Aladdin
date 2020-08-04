f026c2c5cd03a01121e6655fdeba145ecd1e42f3
NinaCalvi <nina@calvifamily.org>
2020-08-04 09:48:03 +0200

    mistake tucker

--- biolink/embeddings/regulariser.py
+++ biolink/embeddings/regulariser.py
@@ -34,15 +34,15 @@ class N3(Regularizer):
     def forward(self, factors):
         norm = 0
         for i,f in enumerate(factors):
-            # if not (self.tucker_weight is None) and (i == 3):
-            #     norm += self.tucker_weight * torch.sum(
-            #         torch.abs(f) ** 3)
-            # else:
-            #     norm += self.weight * torch.sum(
-            #         torch.abs(f) ** 3
-            #     )
-            norm += self.weight * torch.sum(
+            if not (self.tucker_weight is None) and (i == 3):
+                norm += self.tucker_weight * torch.sum(
+                    torch.abs(f) ** 3)
+            else:
+                norm += self.weight * torch.sum(
                     torch.abs(f) ** 3
                 )
+            # norm += self.weight * torch.sum(
+            #         torch.abs(f) ** 3
+            #     )
 
         return norm / factors[0].shape[0]
